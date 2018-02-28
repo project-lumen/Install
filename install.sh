1a75b55494622a67ddbe7811ea0cd3a9a6c5457a
Adrien <33926737+afevre@users.noreply.github.com>
2018-02-28 13:48:41 +0100

    first-commit

---
+++ install.sh
@@ -0,0 +1,193 @@
+########################### MAIN MENU ##############################
+    ####### Installation Virtual box #######
+     read -p "
+    _______________________________________________________________
+    | Menu :                                                        |
+    | 1 - Installer Virtual box                                     |
+    | 2 - Ne pas installer virtual box                              |
+    _______________________________________________________________
+    " menu;
+    if [[ $menu -eq 1 ]]
+    then
+            ####### Votre OS #######
+             read -p "
+            _______________________________________________________________
+            | Menu :                                                        |
+            | 1 - MAC                                                       |
+            | 2 - Linux apt                                                 |
+            _______________________________________________________________
+            " menu;
+            if [[ $menu -eq 1 ]]
+            then
+              wget https://download.virtualbox.org/virtualbox/5.2.6/VirtualBox-5.2.6-120293-OSX.dmg
+            elif [[ $menu -eq 2 ]]
+            then
+              sudo apt install -y virtualbox-qt
+            else
+             echo "Saisie incorrect";
+           fi
+    elif [[ $menu -eq 2 ]]
+    then
+     echo "Non installer";
+    else
+     echo "Saisie incorrect";
+    fi
+
+
+    ####### Installation Vagrant #######
+     read -p "
+    _______________________________________________________________
+    | Menu :                                                        |
+    | 1 - Installer Vagrant                                         |
+    | 2 - Ne pas installer Vagrant                                  |
+    _______________________________________________________________
+    " menu;
+    if [[ $menu -eq 1 ]]
+    then
+          ####### Votre OS #######
+           read -p "
+          _______________________________________________________________
+          | Menu :                                                        |
+          | 1 - MAC                                                       |
+          | 2 - Linux apt                                                 |
+          _______________________________________________________________
+          " menu;
+          if [[ $menu -eq 1 ]]
+          then
+            wget https://releases.hashicorp.com/vagrant/2.0.2/vagrant_2.0.2_x86_64.dmg
+          elif [[ $menu -eq 2 ]]
+          then
+            sudo apt install -y vagrant
+          else
+           echo "Saisie incorrect";
+         fi
+    elif [[ $menu -eq 2 ]]
+    then
+      echo "Non installer";
+    else
+     echo "Saisie incorrect";
+    fi
+
+
+  #### VERIF DOSSIER data  ####
+  if [ ! -d data ];
+  then
+      mkdir data
+      echo -e "
+      \033[44mLe dossier data a été creer !\033[0m"
+  else
+      echo -e "
+      \033[45mUn dossier data existe deja !\033[0m"
+  fi
+
+  #### VERIF DOSSIER project  ####
+  if [ ! -d project ];
+  then
+      mkdir project
+      echo -e "
+      \033[44mLe dossier project a été creer !\033[0m"
+  else
+      echo -e "
+      \033[45mUn dossier project existe deja !\033[0m"
+  fi
+
+
+
+  #### MODIF VAGRANT FILE ####
+
+  #### VERIF VAGRANTFILE ####
+  if [ ! -f Vagrantfile ];
+  then
+
+      echo "
+      # -*- mode: ruby -*-
+      # vi: set ft=ruby :
+      Vagrant.configure(\"2\") do |config|
+      config.vm.box = \"ubuntu/xenial64\"
+      config.vm.synced_folder \"./data\", \"/var/www/html\"
+      config.vm.network \"private_network\", ip: \"192.168.33.10\"
+      config.vm.provision \"shell\", path: \"setup.sh\"
+      end" > Vagrantfile
+
+      echo -e "
+      \033[44mLe fichier Vagrantfile a été généré !\033[0m"
+      echo "Ip connection 192.168.33.10";
+  else
+      echo -e "
+      \033[45mUn Vagrantfile existe déjà dans ce dossier !\033[0m"
+      ####### Votre OS #######
+       read -p "
+      _______________________________________________________________
+      | Menu : Voulez vous le modifier                                |
+      | 1 - Oui                                                       |
+      | 2 - Non                                                       |
+      _______________________________________________________________
+      " menu;
+      if [[ $menu -eq 1 ]]
+      then
+        echo "
+        # -*- mode: ruby -*-
+        # vi: set ft=ruby :
+        Vagrant.configure(\"2\") do |config|
+        config.vm.box = \"ubuntu/xenial64\"
+        config.vm.synced_folder \"./data\", \"/var/www/html\"
+        config.vm.network \"private_network\", ip: \"192.168.33.10\"
+        config.vm.provision \"shell\", path:\"setup.sh\", privileged:false
+        end" > Vagrantfile
+        echo "Ip connection 192.168.33.10";
+      elif [[ $menu -eq 2 ]]
+      then
+        echo "Ok"
+      else
+       echo "Saisie incorrecte";
+     fi
+  fi
+
+  # deplacer wp.sh dans dossier data
+    vagrant up
+    vagrant provision
+ ####### SSH Installation reppo Git/Mongo/lumen #######
+
+    ####### Clone reppo #######
+     read -p "
+    _______________________________________________________________
+    | Menu : Choix branch back-lumen                                |
+    | 1 - master (prod)                                             |
+    | 2 - Develop                                                   |
+    _______________________________________________________________
+    " menu;
+    cd data
+    if [[ $menu -eq 1 ]]
+    then
+      git clone https://github.com/project-lumen/back-lumen.git
+      echo "OK"
+    elif [[ $menu -eq 2 ]]
+    then
+      git clone -b develop https://github.com/project-lumen/back-lumen.git
+      echo "OK"
+    else
+     echo "Saisie incorrecte";
+   fi
+    cd ../
+
+    read -p "
+   _______________________________________________________________
+   | Menu : Choix branch front-electron                            |
+   | 1 - master (Prod)                                             |
+   | 2 - Develop                                                   |
+   _______________________________________________________________
+   " menu;
+   cd project
+   if [[ $menu -eq 1 ]]
+   then
+     git clone https://github.com/project-lumen/front-electron.git
+     echo "OK"
+   elif [[ $menu -eq 2 ]]
+   then
+     git clone -b develop https://github.com/project-lumen/front-electron.git
+     echo "OK"
+   else
+    echo "Saisie incorrecte";
+  fi
+   cd ../
+    vagrant ssh
---
+++ setup.sh
@@ -0,0 +1,112 @@
+
+#cd /home/vagrant/
+
+
+echo "
+ ____________________________________
+|                                    |
+|        UPDATE / UPGRADE            |
+ ____________________________________
+"
+sudo apt-get update && sudo apt-get upgrade -y
+
+
+echo "
+ ____________________________________
+|                                    |
+|               PHP                  |
+ ____________________________________
+"
+echo " *** ADD PHP7.2 *** "
+sudo add-apt-repository ppa:ondrej/php
+echo " *** UPDATE *** "
+sudo apt-get update -y
+
+echo " *** INSTALL PHP 7.2 *** "
+sudo apt-get install php7.2 -y
+echo " *** INSTALL PHP 7.2 PACKAGE  *** "
+sudo apt-get install php7.2-zip -y
+echo " *** INSTALL APACHE 2 / LIBAPACHE 2 *** "
+sudo apt-get install -y apache2 libapache2-mod-php7.2
+echo " *** XML *** "
+
+sudo apt-get install php7.2-xml -y
+
+
+
+    echo -e"
+\033[45m INSTALLATION EXTENSIONS PHP \033[0m"
+
+echo " *** INSTALL PHP 7.2 COMMON *** "
+sudo apt-get install php7.2-common -y
+echo " *** INSTALL PHP 7.2 MBSTRING *** "
+sudo apt-get install php7.2-mbstring -y
+echo " *** INSTALL PHP OPENSSL *** "
+sudo apt-get install openssl -y
+
+
+
+
+# echo "
+#  ____________________________________
+# |                                    |
+# |              MONGO DB              |
+#  ____________________________________
+# "
+# echo " *** ADD MONGO DB *** "
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+# echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+#
+# echo " *** UPDATE *** "
+# sudo apt-get update
+# echo " *** INSTALL MONGO DB *** "
+# sudo apt-get install -y mongodb-org
+
+
+echo "
+ ____________________________________
+|                                    |
+|              COMPOSER              |
+ ____________________________________
+"
+
+echo " *** INSTALL ADD COMPOSER  *** "
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+
+
+echo " *** COMPOSER SETUP *** "
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+echo " *** DEPLACE COMPOSER *** "
+sudo mv composer.phar /usr/local/bin/composer
+
+echo " *** DISPLAY VERSION COMPOSER  *** "
+composer -V
+
+
+echo "
+ ____________________________________
+|                                    |
+|              LUMEN                 |
+ ____________________________________
+"
+
+ #echo " *** REQUIRE LUMEN *** "
+ #composer global require "laravel/lumen-installer"
+ echo " *** INSTALL LUMEN BY COMPOSER *** "
+ #export PATH=$PATH:/home/vagrant/.config/composer/vendor/bin
+ cd /var/www/html
+ composer create-project --prefer-dist laravel/lumen Projet-group
+ #cd /var/www/html
+
+ #echo " *** CREATE PROJECT LUMEN *** "
+ #lumen new projectLumen
+
+ #echo " *** REDIRECTION DOSSIER PROJET  *** "
+ #cd projectLumen
+
+ #echo " *** MaJ COMPOSER  *** "
+ #composer update -vvv
